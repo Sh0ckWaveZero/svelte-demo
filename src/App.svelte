27fdcb7ed62806079a5bdf3db4d1e67e9153e508
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-07-09 11:12:59 +0700

    📝 docs(README.md): update project title to "Learning Svelte + TS + Vite" for clarity and emphasis on learning 🔨 refactor(App.svelte): remove unused imports and styles, reorganize imports for better readability 🔨 refactor(Counter.svelte): add decrement functionality to the counter component, update button styles for better visibility

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  import { Button } from 'flowbite-svelte'
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
+  import Counter from './lib/Counter.svelte'
+  import svelteLogo from './assets/svelte.svg'
 </script>
 
 <main>
@@ -25,25 +23,4 @@
   <div class="card">
     <Counter />
   </div>
-
-  <Button class="!bg-blue-500">Blue Button</Button>
-  <Button class="!bg-green-500">Green Button</Button>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
+  import { Button } from 'flowbite-svelte'
+
   let count: number = 0
   const increment = () => {
     count += 1
   }
+
+  const decrement = () => {
+    count -= 1
+  }
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<p class='mb-3'>{count}</p>
+
+<Button class="!bg-green-500"on:click={increment}>count + </Button>
+<Button class="!bg-yellow-500"on:click={decrement}>decrement -</Button>
